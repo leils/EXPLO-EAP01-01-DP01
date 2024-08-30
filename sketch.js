734dcbb88b25e19b6e024b3f2c46e4c75ff2461f
Leia <e.leiachang@gmail.com>
2024-08-30 15:58:47 -0700

    Animation mode added (#3)
    Created an animation mode, which required restructuring of "modes" as a whole.
    
    * previous drawings animated
    
    * some restructuring
    
    * submitting drawing starts show mode
    
    * timeout show mode
    
    * working flash animation
    
    * add timeout before swapping to show
    
    * adding mid-animation text
    
    * centering buttons
    
    * centered animation prompt text. needs work on modes
    
    * ignoring ds store files
    
    * mode assigning issue
    
    * modes seems to be working, text background added for contrast readability'
    
    * cleaning up mades
    
    ---------
    
    Co-authored-by: Leia Chang <leia.chang@exploratorium.edu>

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ let buttonHeight;
 let promptTextSize = 50; //Gets rewritten based on window width 
 const drawPromptText = "Do you see something in this image? Draw it!";
 const showPromptText = "Tap the screen to start drawing";
+const afterSubmitText = "Great! Let's see what other people drew.";
 
 /*--------------------- Drawings variables -------------------------*/
 /*
@@ -31,12 +32,85 @@ let currentColorIndex = 0;
 /* There are two modes; drawing mode, and showing mode 
  * Drawing + drawing IO, image navigation, only available in drawing mode 
  */ 
-let drawMode = true; 
-let tapForEscape = false;
+const Modes = Object.freeze({
+  DRAW: 0,
+  SUBMIT: 1,
+  SHOW: 2
+});
+
+let currentMode = Modes.DRAW;
+
+let drawingsForCurrentImage = [];
+let currentImageDrawingIndex = 0;
+let drawingOpacity = 0;
+let drawingColor;
+let flashOpacity = 0;
+
+//-------------------- Show Modes ---------------------//
+function toggleMode() {
+  if (currentMode == Modes.DRAW) { // draw -> submit -> show 
+    for (b of allButtons) { // hide all buttons
+      b.hide();
+    }
+    currentMode = Modes.SUBMIT;
+    resetBackground();
+
+    setTimeout(() => { showModeSetup(); },2000); //goes to ShowMode in 2 seconds
+
+    // Set a timeout to return to draw mode after 30 seconds
+    setTimeout(() => {
+      if(currentMode == Modes.SHOW) {
+        toggleMode();
+      }
+    }, 30000)
+
+  } else { // show mode -> draw mode 
+    clearCanvas();
+    for (b of allButtons) { // show all buttons
+      b.show();
+    }
+    currentMode = Modes.DRAW
+
+    showModeTeardown();
+  }
+}
+
+function showModeSetup() {
+  resetBackground();
+  drawingsForCurrentImage = drawingList.filter(d => d.imgIndex == currentImageIndex);
+  currentImageDrawingIndex = 0;
+  drawingOpacity = 0;
+  drawingColor = color(drawingsForCurrentImage[currentImageDrawingIndex].colorStr);
+
+  currentMode = Modes.SHOW;
+}
+
+function showModeTeardown() {
+  currentImageDrawingIndex = 0;
+  drawingOpacity = 0;
+  drawingsForCurrentImage = [];
+}
+
+function renderShowModeFrame() {
+  resetBackground();
+  
+  push();
+  let drawing = drawingsForCurrentImage[currentImageDrawingIndex];
+  drawingColor.setAlpha(drawingOpacity);
+  stroke(drawingColor);
+  drawStrokes(drawing.strokes);
+  
+  if (drawingOpacity < 255) {
+    drawingOpacity+=2;
+  } else {
+    getNextDrawing();
+  }
+  pop();
+}
 
 /*--------------------- Buttons -------------------------*/
-let drawModeButtons = [];
-const buttonOffset = 40;
+let allButtons = [];
+const buttonOffset = 100;
 
 const buttonInfo = [ 
   {
@@ -47,10 +121,6 @@ const buttonInfo = [
     label: "Submit",
     clickFunct: submitDrawing,
     className: "submitButton"
-  }, 
-  {
-    label: "Show All Drawings",
-    clickFunct: toggleMode
   }, 
   {
     label: "Next Image",
@@ -100,46 +170,67 @@ function fetchJSONData() {
 function setup() {
   createMetaTag();
   createCanvas(window.innerWidth, window.innerHeight);
-  buttonHeight = window.innerHeight - 130;
+
+  buttonHeight = window.innerHeight - 120;
   promptTextSize = Math.floor(window.innerWidth/21);
+  textSize(promptTextSize);
+  textAlign(CENTER);
+  strokeWeight(setStrokeWeight);
+  stroke(colorList[currentColorIndex]);
 
   resetBackground();
   buttonInit();
-  strokeWeight(setStrokeWeight);
-  stroke(colorList[currentColorIndex]);
 }
 
-
 function draw() {
-  drawPrompt();
+  if (currentMode == Modes.SHOW) {
+    renderShowModeFrame();
+  }
+  handleFlashAnimation();
+  if (currentMode != Modes.SUBMIT) { drawPrompt(); }
+  console.log(currentMode);
 }
 
 function buttonInit() {
-  let spaceOffset = buttonOffset;
+  let totalWidth = 0;
   for (var i=0; i < buttonInfo.length; i++) {
     let bInfo = buttonInfo[i];
     let newButton = createButton(bInfo.label);
     if (bInfo.hasOwnProperty("className")) {
       newButton.class(bInfo.className);
     }
-    newButton.position(spaceOffset, buttonHeight);
     newButton.mousePressed(bInfo.clickFunct);
 
-    spaceOffset += (newButton.width + buttonOffset);
-    drawModeButtons.push(newButton);
+    totalWidth += newButton.width;
+    allButtons.push(newButton);
   }
+
+  // centering the buttons on-screen
+  totalWidth += (allButtons.length - 1) * buttonOffset;
+  let spaceOffset = (window.innerWidth - totalWidth)/2;
+
+  for (b of allButtons) {
+    b.position(spaceOffset, buttonHeight);
+    spaceOffset += (buttonOffset + b.width);
+  }
+
 }
 
 function drawPrompt() {
+  // TODO incorporate submit mode into promp drawing for clarity
   push();
-  textSize(promptTextSize);
+  fill("black");
+  noStroke();
+  rectMode(CENTER);
+  rect(window.innerWidth/2, window.innerHeight - 100, window.innerWidth, 200, 30);
+
   strokeWeight(3);
   stroke('black');
   fill('yellow');
-  if (drawMode) {
-    text(drawPromptText, 40, window.innerHeight-150);
+  if (currentMode == Modes.DRAW) {
+    text(drawPromptText, window.innerWidth/2, window.innerHeight-150);
   } else {
-    text(showPromptText, 150, window.innerHeight-100);
+    text(showPromptText, window.innerWidth/2, window.innerHeight-100);
   }
   pop();
 }
@@ -165,38 +256,10 @@ function drawStrokes(slist) {
   }
 }
 
-function toggleMode() {
-  if (drawMode) { // draw mode -> show mode
-    for (b of drawModeButtons) { // hide all buttons
-      b.hide();
-    }
-    resetBackground();
-    showAllDrawings();
-    drawMode = false;
-
-    // Needs some delay, otherwise the tap will be registered due to button click and toggle immediately
-    setTimeout(() => { 
-      tapForEscape = true;
-    }, 200);
-  } else { // show mode -> draw mode 
-    clearCanvas();
-    for (b of drawModeButtons) { // show all buttons
-      b.show();
-    }
-    tapForEscape = false;
-    drawMode = true;
-  }
-}
-
-function showAllDrawings() {
-  let relevantDrawings = drawingList.filter(d => d.imgIndex == currentImageIndex); 
-
-  for (var drawing of relevantDrawings) {
-    push();
-    stroke(drawing.colorStr);
-    drawStrokes(drawing.strokes);
-    pop();
-  }
+function getNextDrawing() {
+  currentImageDrawingIndex = currentImageDrawingIndex < drawingsForCurrentImage.length - 1 ? currentImageDrawingIndex + 1 : 0;
+  drawingOpacity = 0;
+  drawingColor = color(drawingsForCurrentImage[currentImageDrawingIndex].colorStr);
 }
 
 function nextImage() {
@@ -207,20 +270,19 @@ function nextImage() {
 function changeColor() {
   currentColorIndex =
     currentColorIndex >= colorList.length - 1 ? 0 : currentColorIndex + 1;
-  console.log(currentColorIndex);
 
   stroke(colorList[currentColorIndex]);
 }
 
 //-------------------- Drawing & Mouse ---------------------//
 function mouseReleased() {
-  if (drawMode) {
+  if (currentMode == Modes.DRAW) {
     endStroke();
   }
 }
 
 function touchEnded() {
-  if (drawMode) {
+  if (currentMode == Modes.DRAW) {
     endStroke();
   }
 }
@@ -230,14 +292,12 @@ function touchStarted() {
   // This is a hack to make sure the stroke starts where touch starts 
   pmouseX = mouseX;
   pmouseY = mouseY;
-  if (tapForEscape) {
-    console.log("toggling");
+  if (currentMode == Modes.SHOW) {
     toggleMode();
   }
 }
 
 function endStroke() {
-  console.log('ending stroke');
   // commit this stroke to the StrokeList
   if (currentStroke.length > 1) {
     strokeList.push(currentStroke);
@@ -249,11 +309,10 @@ function endStroke() {
 
 // mouseDragged runs on touch on mobile, so long as touchMoved is not defined
 function mouseDragged() {
-  if (drawMode) {
+  if (currentMode == Modes.DRAW) {
     // add the first point to the stroke if not dragged yet
     // only useful for mouseDrag, not required for touch 
     if (currentStroke.length == 0) {
-      console.log('taking last point ', pmouseX, pmouseY)
       currentStroke.push({ x: pmouseX, y: pmouseY });
     }
 
@@ -267,26 +326,51 @@ function undo() {
     strokeList.pop();
     resetBackground();
     drawStrokes(strokeList);
-    console.log("undid");
   }
 }
 
 //-------------------- Canvas ---------------------//
-
 function clearCanvas() {
   strokeList = [];
   resetBackground();
-  console.log("cleared");
 }
 
 function submitDrawing() {
-  console.log("Submitting Drawing");
   if (strokeList.length > 0) {
     let d = new Drawing(currentImageIndex, colorList[currentColorIndex], strokeList);
     drawingList.push(d);
+
+    flashOpacity = 255;
     strokeList = [];
     resetBackground();
     changeColor();
+    toggleMode();
+  }
+}
+
+function handleFlashAnimation() {
+  if (flashOpacity > 0) {
+    push();
+    resetBackground();
+    noStroke();
+    fill('black');
+    rectMode(CENTER);
+    rect(window.innerWidth/2, window.innerHeight/2 - (promptTextSize/3), window.innerWidth, 100, 30);
+    strokeWeight(3);
+    stroke('black');
+    fill('yellow');
+    textAlign(CENTER);
+    text(afterSubmitText, window.innerWidth/2, window.innerHeight/2);
+    pop();
+
+    push();
+    noStroke();
+    let flashColor = color("white");
+    flashColor.setAlpha(flashOpacity);
+    fill(flashColor);
+    rect(0,0,window.innerWidth, window.innerHeight);
+    flashOpacity = flashOpacity - 10;
+    pop(0);
   }
 }
 
@@ -295,6 +379,8 @@ function submitDrawing() {
 function saveDrawingsToJson() {
   saveJSON(drawingList, 'drawings.json');
 }
+
+
 
 //-------------------- Admin ---------------------//
 function keyPressed() {
@@ -307,7 +393,7 @@ function keyPressed() {
   } else if (key == "r") {
     resetBackground();
   } else if (key == "a") {
-    showAllDrawings();
+    toggleMode();
   } else if (key == "n") {
     nextImage();
   } else if (key == "1") {
